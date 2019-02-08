2a3e92e7c57247033802c61aecd7ea7ca9abf137
Anas Neumann <anas.neumann.isamm@gmail.com>
2019-02-07 21:58:31 -0500

    design graphique partie 2

---
+++ mqt-8100-ui/src/app/ts/components/averagetest/AverageTest.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, NgModule, TemplateRef, Renderer } from '@angular/core';
+import { GenericComponent } from '../GenericComponent';
+import { LocalService } from '../../services/LocalService';
+import { MessageService } from '../../services/MessageService';
+import { WebService } from '../../services/WebService';
+import { ActivatedRoute, Router} from '@angular/router';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+
+/*
+ * Component principal de l'application
+ * @author Anas Neumann <anas.neumann.isamm@gmail.com>
+ * @since 07/02/2019
+ *  * @version 1.0
+ */
+@Component({
+  selector: 'mqt-average',
+  templateUrl: 'averageTest.html',
+  styleUrls: ['averageTest.css']
+})
+export class AverageTest extends GenericComponent {
+
+    /**
+     * Constructor
+     * @param localService
+     * @param i18NService
+     */
+     constructor(private modalService: BsModalService, private service : WebService, private messageService : MessageService, private renderer: Renderer, private localService : LocalService) {       
+        super(messageService, renderer, localService);
+     }
+
+}
