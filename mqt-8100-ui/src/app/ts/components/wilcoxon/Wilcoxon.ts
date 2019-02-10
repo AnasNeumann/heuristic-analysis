87efd3f274a4d04330a8d5d539600e4a81a77acc
Anas Neumann <anas.neumann.isamm@gmail.com>
2019-02-09 20:50:34 -0500

    complete wilcoxon weighted rank test features

--- mqt-8100-ui/src/app/ts/components/wilcoxon/Wilcoxon.ts
+++ mqt-8100-ui/src/app/ts/components/wilcoxon/Wilcoxon.ts
@@ -16,9 +16,15 @@ import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 @Component({
   selector: 'mqt-wilcoxon',
   templateUrl: 'wilcoxon.html',
-  styleUrls: ['wilcoxon.css']
+  styleUrls: ['wilcoxon.css', '../home/home.css', '../averagetest/averageTest.css']
 })
 export class Wilcoxon extends GenericComponent {
+    loaded : boolean = false;
+    data : any[] = [];
+    order : any[] = [];
+    heuristics : any[] = [];
+    xAxisLabel = "Heursitiques";
+    yAxisLabel = "Total rejets de Ho";
 
     /**
      * Constructor
@@ -27,6 +33,27 @@ export class Wilcoxon extends GenericComponent {
      */
      constructor(private modalService: BsModalService, private service : WebService, private messageService : MessageService, private renderer: Renderer, private localService : LocalService) {       
         super(messageService, renderer, localService);
+        this.service.get("/analyzer/wilcoxon", {}).then(response =>{
+            for(var h in response.many){
+                var newValue = {
+                      name : response.many[h].h,
+                      value : 0
+                };
+                for(var t in response.many[h].tests){
+                    var result = response.many[h].tests[t];
+                    if(result.value > result.w95 || result.value > result.w99){
+                        newValue.value ++;
+                        this.order.push({
+                           inf : response.many[h].h,
+                           sup : result.name
+                        });
+                    }
+                }
+                this.data.push(newValue);
+            }
+            this.heuristics = response.many;
+            this.loaded = true;
+        });
      }
 
 }
