9164196555b7adb2508ed7fd4d5b410e3270da80
Anas Neumann <anas.neumann.isamm@gmail.com>
2019-03-09 00:12:39 -0500

    add javadoc

--- gso-7085/src/com/gso/boot/Main.java
+++ gso-7085/src/com/gso/boot/Main.java
@@ -2,9 +2,19 @@ package com.gso.boot;
 
 import com.gso.engines.ParsingFileEngine;
 
+/**
+ * Classe principale du projet
+ * @author Anas Neumann <anas.neumann.isamm@gmail.com>
+ * @author Safwan Al-Shabakji
+ * @since 08/03/2019
+ * @version 1.0
+ */
 public class Main {
 
-	
+	/**
+	 * Main method
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		ParsingFileEngine.BuildInstance("instance1.xlsx");
 	}
