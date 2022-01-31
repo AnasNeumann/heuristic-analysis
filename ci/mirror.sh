80d15d39bcdafed7c2942ae12292408c5cdaae95
Anas Neumann <anas.neumann.isamm@gmail.com>
2022-01-31 17:39:25 +0000

    Update mirror.sh

--- ci/mirror.sh
+++ ci/mirror.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REPO_PATH="${PROJECT_HOME}/heuristic-learning/"
+REPO_PATH="${PROJECT_HOME}/heuristic-analysis/"
 
 cd "${REPO_PATH}" && git pull origin master || :
 git push github master 
